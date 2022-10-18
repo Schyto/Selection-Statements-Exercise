c78f24e505a28da6a0fe3cd47396b7545d6ff1b7
Michael Doyle <doyle.m@me.com>
2022-10-18 09:39:45 -0500

    Update Program.cs

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Number Guessing Game
+            var r = new Random();
+            //var favNumber = r.Next(1, 10);
+            var favNumber = 5;
+
+            Console.WriteLine("Pick a number from 1 - 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInpute < favNumber)
+            {
+                COnsole.WriteLine($"{userInpute} is too low");
+            }
+            else if(userInput > favNumber) 
+            {
+                Console.WriteLine($"{ userInpute} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
